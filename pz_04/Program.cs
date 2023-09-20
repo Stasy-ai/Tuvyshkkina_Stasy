47b00fe916776e5a19073fe2e54b25884a1e5699
Valp Cat <nnaska888@gmail.com>
2023-09-20 16:43:34 +0300

    практика 04

---
+++ pz_04/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace pz_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
+/*задание 5
+            Console.WriteLine("значение 2х переменных");
+            int i, j;
+            for (i = 0, j=55; Math.Abs(i-j) >= 9; i++, j--)
+            {
+               Console.WriteLine(" i = " +i+ " j = " +j);
+            }
+/задание 3
+  Console.WriteLine("# много раз");
+            int n = 5;
+            int m = 8;
+            for ( int i = 0; i<m; i++)
+            {
+                for ( int j = 0; j < n; j++)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+            }
+/задание 2
+            Console.WriteLine("Вывод символов в авфавитном порядке");
+            for (char c = 'к'; c <= 'с'; c++)
+            {
+                Console.Write(c);
+            }
+            
+/задача 1
+ Console.WriteLine("вывод диапозона"); 
+  int start = -100;
+  int end = 0;
+  int step = 2;
+  for (int i = start; i <= end; i+= step)
+  {
+      Console.WriteLine(i);
+  }
+  Console.ReadLine();*/
